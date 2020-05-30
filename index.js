34464782d540c268620741114a51058adc10425d
Evan hameed <evanhameed9966@gmail.com>
2020-05-30 21:56:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+
+let driver = {};
+function updateDriverWithKeyAndValue(obj,key,value){
+  let newDriver = {...obj}
+  newDriver[key]=value;
+
+  return newDriver;
+}
